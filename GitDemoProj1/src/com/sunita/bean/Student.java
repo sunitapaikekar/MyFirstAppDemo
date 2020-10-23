661856f2fd5a224e4b581bdb4ee8a052ebd8d468
Sunita <sunitapcse@gmail.com>
2020-10-23 19:00:39 +0530

    Dev1 Created Test class and changes sone in  Student class

--- GitDemoProj1/src/com/sunita/bean/Student.java
+++ GitDemoProj1/src/com/sunita/bean/Student.java
@@ -1,5 +1,8 @@
 package com.sunita.bean;
 
 public class Student {
+	public static void main(String[] args) {
+		System.out.println("Main method created in Student ");
+	}
 
 }
---
+++ GitDemoProj1/src/com/sunita/test/UserTest.java
@@ -0,0 +1,10 @@
+package com.sunita.test;
+
+public class UserTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Dev1 created User Test class");
+	}
+
+}
